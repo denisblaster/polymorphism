39df5f605341c4a52fddbbb35e5ee3dbbf9b144d
denisblaster <59642354+denisblaster@users.noreply.github.com>
2020-10-02 10:43:30 +0200

    Add files via upload
    owo

---
+++ polymorphism/polymorphism/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WorldInhibitant[] worldlist = { new england(), new russia(), new WorldInhibitant() };
+
+            foreach (var worlds in worldlist)
+            {
+                worlds.worldsounds();
+            }
+        }
+    }
+    class WorldInhibitant
+    {
+        virtual public void worldsounds()
+        {
+            Console.WriteLine("I live in the world");
+        }
+    }
+    class england : WorldInhibitant
+    {
+        override public void worldsounds()
+        {
+            Console.WriteLine("england says BRITISH FOREVER");
+        } 
+    }
+    class russia : WorldInhibitant
+    {
+        public override void worldsounds()
+        {
+            Console.WriteLine("russia says nyohohohohohohoo");
+        }
+    }
+
+}
